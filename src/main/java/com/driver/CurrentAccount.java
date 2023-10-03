f7bb9a81a541bc6dd6f6cd3ac3b4de176abb3c7e
Surya <49099412+suryapandiaraj@users.noreply.github.com>
2023-10-03 20:54:20 +0530

    Update CurrentAccount.java

--- src/main/java/com/driver/CurrentAccount.java
+++ src/main/java/com/driver/CurrentAccount.java
@@ -5,7 +5,8 @@ public class CurrentAccount extends BankAccount{
 
     public CurrentAccount(String name, double balance, String tradeLicenseId) throws Exception {
         // minimum balance is 5000 by default. If balance is less than 5000, throw "Insufficient Balance" exception
-
+        super(name, 0, 5000);
+        this.tradeLicenseId = tradeLicenseId;
     }
 
     public void validateLicenseId() throws Exception {
@@ -13,7 +14,11 @@ public class CurrentAccount extends BankAccount{
         // If the license Id is valid, do nothing
         // If the characters of the license Id can be rearranged to create any valid license Id
         // If it is not possible, throw "Valid License can not be generated" Exception
-
+        for(int i=0;i<this.tradeLicenseId.length() - 1;i++){
+            if(this.tradeLicenseId.charAt(i) == this.tradeLicenseId.charAt(i+1)){
+                throw new Exception("Invalid ID");
+            }
+        }
     }
 
 }
